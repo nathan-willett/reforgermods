7e9cf679b3c400595c08c7f4eec64b7b19fee85e
Nathan Willett <natewillett1996@gmail.com>
2024-12-17 21:03:28 -0800

    removed bacon files from reforger repo

--- BaconLoadoutEditor/GunBuilderUI_Modded_ArsenalManagerComponent.c
+++
@@ -1,5 +0,0 @@
-modded class SCR_ArsenalManagerComponent {
-	bool GunBuilderUI_CanSaveLoadout(int playerId, GameEntity characterEntity, FactionAffiliationComponent playerFactionAffiliation, SCR_ArsenalComponent arsenalComponent, bool sendNotificationOnFailed) {
-		return CanSaveLoadout(playerId, characterEntity, playerFactionAffiliation, arsenalComponent, sendNotificationOnFailed);
-	}
-}
--- BaconLoadoutEditor/GunBuilderUI_Modded_MapMarkers.c
+++
@@ -1,7 +0,0 @@
-[BaseContainerProps(), SCR_MapMarkerTitle()]
-modded class SCR_MapMarkerEntrySquadLeader: SCR_MapMarkerEntryDynamic
-{
-	void BaconLoadoutEditor_UpdateMarkerTarget(int playerId) {
-		UpdateMarkerTarget(playerId);
-	}
-}
